5136df18340f40821fb9a4837bbbb559fd190435
Lovish Jindal <88631544+Lovish0181@users.noreply.github.com>
2021-09-14 02:33:26 +0530

    Add files via upload

---
+++ Resume.php
@@ -0,0 +1,178 @@
+<html>
+<head>
+    <title>signup</title>
+    <style>
+        * {
+  max-width: 600px;
+  margin: auto;
+}
+div {
+  border-radius: 5px;
+}
+
+#header {
+  height: 40px;
+  width: 100%;
+  background-color: #2f2b47;
+  position: fixed;
+  z-index: 1;
+}
+
+#title {
+  margin-left: 3%;
+}
+
+#footer {
+  height: 50px;
+  width: 100%;
+  background-color: #2f2b47;
+  clear: both;
+  position: relative;
+}
+
+.left {
+  height: 1300px;
+  width: 45px;
+  background-color: #e0eeee;
+  float: left;
+  position: fixed;
+}
+
+.right {
+  height: 1300px;
+  width: 45px;
+  background-color: #e0eeee;
+  float: right;
+  position: inherit;
+}
+
+.stuff {
+  display: inline-block;
+  margin-top: 6px;
+  margin-left: 55px;
+  width: 75%;
+  height: 1000px;
+}
+
+p,
+
+.head {
+  font-size: 15px;
+}
+
+#name {
+  font-family: Sacramento;
+  float: right;
+  margin-top: 10px;
+  margin-right: 4%;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+@media only screen and (max-width: 430px) {
+  .left,
+  .right {
+    display: none;
+  }
+  .stuff {
+    width: 100%;
+    margin-left: 10px;
+  }
+}
+    </style>
+</head>
+
+<body>
+    <div id="header"></div>
+<div class="left"></div>
+<div class="stuff">
+  <br><br>
+  <h1>Lovish Jindal</h1>
+  
+  <h4>Student/Web-Developer</h4>
+ <br>
+    An Inspiring engineer looking for an organization where i canleverage my knowledge and gain experience while contributing tothe growth of the organization. Also, I look forward to establishmyself by adapting to new technologies as well.
+  <hr/>
+  <br>
+  <p class="head">EDUCATION</p>
+  <h4>Thapar Institute of Engineering and Technology</h4>
+  <li>August 2019 - Present</li>
+B.E- Electronics and Computer Engineering
+<li>CGPA-7.37</li>
+  
+  <h4>Sanawar an Institute for Children's (XII)</h4>
+  <li>March 2016- March 2017
+</li>
+  Non-Medical
+<li>Percentage-81.2</li>
+  <h4>St. Joseph's Convent Sen. Sec. School (X)</h4>
+  <li>March 2014- March 2016</li>
+Schooling
+<li>CGPA-9.8</li>
+  <br>
+  <p class="head">SKILLS</p>
+  <ul>
+    <li>C/C++</li>
+    <li>FULL STACK WEB-DEVELOPMENT(HTML5, CSS3,Bootstrap, JSON, AJAX, JavaScript,PHP-Mysql, JQuery, AngularJS)</li>
+    <li>JAVA/J2SE</li>
+  </ul>
+  <br>
+  <p class="head">PROJECTS</p>
+  <ul>
+    <a href="https://github.com/Lovish0181/Easy-Work-Services">
+      <h4>Man Power Services
+(Web-Application)</h4>
+      </a>
+     <li>Easy work services is three module based website that help endusers. To find workers, service man or any type of labor withintheir locality. This reduces the effort to go out and find theworker and now you can find workers from the comfort of your home.</li>
+      <li>Developed Using Technologies: HTML5, CSS3, Bootstrap,JSON, AJAX, JavaScript, PHP-Mysql, JQuery, AngularJS.</li>
+        
+        <a href="https://github.com/Lovish0181/Newspaper-Agency-Assistant-Java">
+          <h4>Newspaper Agency Assistant
+            (Java-Application)</h4>
+            </a>
+      <li>This is a java project to solve the problem of gathering andmaintaining data in a Newspaper Agency. What happens is thatin a Newspaper Agency the data of customers and Hawkers ismaintained manually.</li>
+          <li>Developed using Technologies: J2SE+JavaFX, MYSQL
+.</li>
+      <a href="https://github.com/Lovish0181/Property-Dealer-Manager">
+          <h4>Property Dealer Manager
+(Java-Application)         </h4>
+            </a>
+      <li>The main purpose to develop this Property Dealer Manager Javaproject is to manage the data of both Buyer and Seller. TheProject has a feature of converting data to an excel sheet.</li>
+          <li>Developed using Technologies: J2SE+JavaFX, MYSQL
+.</li>
+  </ul>
+  <br>
+  <p class="head">LANGUAGES</p>
+  <ul>
+    English
+    <li>
+Full Professional Proficiency</li>
+    Hindi
+    <li>
+Full Professional Proficiency</li>
+  </ul>
+  <br>
+  <p class="head">EXTRA CURRICULAR ACTIVITIES</p>
+  <ul>
+    <h4>Discipline Head
+    </h4>
+    YWC (Youth Welfare Club)
+  </ul>  <br>
+  <p class="head">CERTIFICATES</p>
+  <ul>
+          <li>C/C++ (2 months Training)</li>
+          <li>Full-Stack Web Development (2 monthsTraining)</li>
+    <li>JAVA/J2SE (3 months Training)</li>
+    </ul><br>
+  <p class="head">HOBBIES/INTERESTS</p>
+  <ul>
+  Playing Cricket, Football, Gymnasium,
+Photography, Travelling.</ul>
+
+</div>
+<div class="right"></div>
+<div id="footer">
+</body></html>
